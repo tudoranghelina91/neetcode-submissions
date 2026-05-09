253a5cde48f3b0dc7b67937de3e629b64fe0bb1e
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2026-05-09 18:03:04 +0300

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,28 @@
+"""
+Build key by counting char occurences - construct key in format a3b5z2
+"""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        hashmap = defaultdict(list)
+
+        for s in strs:
+            freq = [0] * ((ord('z') - ord('a')) + 1)
+
+            for c in s:
+                freq[ord(c) - ord('a')] += 1
+
+            key = ""
+
+            for i in range(len(freq)):
+                if freq[i] > 0:
+                    key = key + str(chr(i)) + str(freq[i])
+            
+            hashmap[key].append(s)
+
+        output = []
+
+        for k, v in hashmap.items():
+            output.append(v)
+
+        return output
