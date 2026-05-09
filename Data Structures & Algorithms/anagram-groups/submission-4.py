f9e94fbc36fbe14649d5a2745ee7cf9df8f4b555
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2026-05-09 17:58:33 +0300

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,31 @@
+"""
+Build the key by sorting the string and constructing hashmap key with count
+"""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        hashmap = {}
+
+        for s in strs:
+            freq = [0] * ((ord('z') - ord('a')) + 1)
+
+            for c in s:
+                freq[ord(c) - ord('a')] += 1
+
+            key = ""
+
+            for i in range(len(freq)):
+                if freq[i] > 0:
+                    key = key + str(chr(i)) + str(freq[i])
+
+            if key not in hashmap:
+                hashmap[key] = []
+            
+            hashmap[key].append(s)
+
+        output = []
+
+        for k, v in hashmap.items():
+            output.append(v)
+
+        return output
