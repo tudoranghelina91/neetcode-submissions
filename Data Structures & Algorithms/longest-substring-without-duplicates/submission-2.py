91978c0829203ba995377ffa11b4580dc78959e0
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2026-05-25 16:38:27 +0300

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    """
+    Hashset. Iterate and remove occurences of current characters from the set
+    """
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charset = set()
+        l = 0
+        result = 0
+
+        for r in range(len(s)):
+            while s[r] in charset:
+                charset.remove(s[l])
+                l += 1
+            charset.add(s[r])
+
+            result = max(result, r - l + 1)
+
+        return result
