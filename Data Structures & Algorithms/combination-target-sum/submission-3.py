c32c10ff04c5a0c415cc98128f7740f9c588e5b7
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2026-06-08 15:11:13 +0300

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        solution = []
+        result = []
+        
+        def backtrack(i, crt_sum):
+            if crt_sum == target:
+                result.append(solution[:])
+                return
+            
+            if crt_sum > target or i == len(nums):
+                return
+            
+            backtrack(i + 1, crt_sum)
+
+            solution.append(nums[i])
+
+            backtrack(i, crt_sum + nums[i])
+            solution.pop()
+
+        backtrack(0, 0)
+
+        return result
