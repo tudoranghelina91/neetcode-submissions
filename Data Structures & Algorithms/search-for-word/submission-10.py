9b5e848c2413a325e5536eab165768457af8d31b
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2026-06-09 14:55:18 +0300

    Add: search-for-word - submission-10

---
+++ Data Structures & Algorithms/search-for-word/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n = len(board)
+        m = len(board[0])
+        directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+        def backtrack(i, j, k):
+            if k == len(word):
+                return True
+
+            if i < 0 or i == n or j < 0 or j == m or board[i][j] != word[k] or board[i][j] == '#':
+                return False
+
+            result = False
+
+            # - sharp sign means this position was visited
+            board[i][j] = '#'
+
+            for d in directions:
+                if backtrack(i + d[0], j + d[1], k + 1) == True:
+                    result = True
+
+            board[i][j] = word[k]
+
+            return result
+
+        for i in range(n):
+            for j in range(m):
+                if backtrack(i, j, 0) == True:
+                    return True
+
+        return False
